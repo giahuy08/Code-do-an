36b0f427332f8ba19ba2bfc602c412c94765990b
giahuy08 <49060880+giahuy08@users.noreply.github.com>
2019-12-14 14:52:06 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Puzzle
+{
+    partial class frmPuzzelGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbPuzzleBox = new System.Windows.Forms.GroupBox();
+            this.pbx1 = new System.Windows.Forms.PictureBox();
+            this.pbx2 = new System.Windows.Forms.PictureBox();
+            this.pbx3 = new System.Windows.Forms.PictureBox();
+            this.pbx4 = new System.Windows.Forms.PictureBox();
+            this.pbx5 = new System.Windows.Forms.PictureBox();
+            this.pbx6 = new System.Windows.Forms.PictureBox();
+            this.pbx7 = new System.Windows.Forms.PictureBox();
+            this.pbx8 = new System.Windows.Forms.PictureBox();
+            this.pbx9 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gbPuzzleBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbPuzzleBox
+            // 
+            this.gbPuzzleBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbPuzzleBox.BackColor = System.Drawing.Color.White;
+            this.gbPuzzleBox.Controls.Add(this.pbx1);
+            this.gbPuzzleBox.Controls.Add(this.pbx2);
+            this.gbPuzzleBox.Controls.Add(this.pbx3);
+            this.gbPuzzleBox.Controls.Add(this.pbx4);
+            this.gbPuzzleBox.Controls.Add(this.pbx5);
+            this.gbPuzzleBox.Controls.Add(this.pbx6);
+            this.gbPuzzleBox.Controls.Add(this.pbx7);
+            this.gbPuzzleBox.Controls.Add(this.pbx8);
+            this.gbPuzzleBox.Controls.Add(this.pbx9);
+            this.gbPuzzleBox.ForeColor = System.Drawing.Color.DarkRed;
+            this.gbPuzzleBox.Location = new System.Drawing.Point(23, 25);
+            this.gbPuzzleBox.Margin = new System.Windows.Forms.Padding(4);
+            this.gbPuzzleBox.Name = "gbPuzzleBox";
+            this.gbPuzzleBox.Padding = new System.Windows.Forms.Padding(4);
+            this.gbPuzzleBox.Size = new System.Drawing.Size(488, 491);
+            this.gbPuzzleBox.TabIndex = 1;
+            this.gbPuzzleBox.TabStop = false;
+            this.gbPuzzleBox.Text = "PuzzleBox";
+            this.gbPuzzleBox.Enter += new System.EventHandler(this.Form1_Load);
+            // 
+            // pbx1
+            // 
+            this.pbx1.Location = new System.Drawing.Point(23, 38);
+            this.pbx1.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx1.Name = "pbx1";
+            this.pbx1.Size = new System.Drawing.Size(145, 146);
+            this.pbx1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx1.TabIndex = 0;
+            this.pbx1.TabStop = false;
+            this.pbx1.Click += new System.EventHandler(this.Swap);
+            // 
+            // pbx2
+            // 
+            this.pbx2.Location = new System.Drawing.Point(174, 38);
+            this.pbx2.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx2.Name = "pbx2";
+            this.pbx2.Size = new System.Drawing.Size(145, 146);
+            this.pbx2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx2.TabIndex = 0;
+            this.pbx2.TabStop = false;
+            this.pbx2.Click += new System.EventHandler(this.Swap);
+            // 
+            // pbx3
+            // 
+            this.pbx3.Location = new System.Drawing.Point(324, 38);
+            this.pbx3.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx3.Name = "pbx3";
+            this.pbx3.Size = new System.Drawing.Size(145, 146);
+            this.pbx3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx3.TabIndex = 0;
+            this.pbx3.TabStop = false;
+            this.pbx3.Click += new System.EventHandler(this.Swap);
+            // 
+            // pbx4
+            // 
+            this.pbx4.Location = new System.Drawing.Point(23, 189);
+            this.pbx4.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx4.Name = "pbx4";
+            this.pbx4.Size = new System.Drawing.Size(145, 146);
+            this.pbx4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx4.TabIndex = 0;
+            this.pbx4.TabStop = false;
+            this.pbx4.Click += new System.EventHandler(this.Swap);
+            // 
+            // pbx5
+            // 
+            this.pbx5.Location = new System.Drawing.Point(174, 189);
+            this.pbx5.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx5.Name = "pbx5";
+            this.pbx5.Size = new System.Drawing.Size(145, 146);
+            this.pbx5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx5.TabIndex = 0;
+            this.pbx5.TabStop = false;
+            this.pbx5.Click += new System.EventHandler(this.Swap);
+            // 
+            // pbx6
+            // 
+            this.pbx6.Location = new System.Drawing.Point(324, 189);
+            this.pbx6.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx6.Name = "pbx6";
+            this.pbx6.Size = new System.Drawing.Size(145, 146);
+            this.pbx6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx6.TabIndex = 0;
+            this.pbx6.TabStop = false;
+            this.pbx6.Click += new System.EventHandler(this.Swap);
+            // 
+            // pbx7
+            // 
+            this.pbx7.Location = new System.Drawing.Point(23, 339);
+            this.pbx7.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx7.Name = "pbx7";
+            this.pbx7.Size = new System.Drawing.Size(145, 146);
+            this.pbx7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx7.TabIndex = 0;
+            this.pbx7.TabStop = false;
+            this.pbx7.Click += new System.EventHandler(this.Swap);
+            // 
+            // pbx8
+            // 
+            this.pbx8.Location = new System.Drawing.Point(174, 339);
+            this.pbx8.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx8.Name = "pbx8";
+            this.pbx8.Size = new System.Drawing.Size(145, 146);
+            this.pbx8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx8.TabIndex = 0;
+            this.pbx8.TabStop = false;
+            this.pbx8.Click += new System.EventHandler(this.Swap);
+            // 
+            // pbx9
+            // 
+            this.pbx9.Location = new System.Drawing.Point(324, 338);
+            this.pbx9.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx9.Name = "pbx9";
+            this.pbx9.Size = new System.Drawing.Size(145, 146);
+            this.pbx9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx9.TabIndex = 0;
+            this.pbx9.TabStop = false;
+            this.pbx9.Click += new System.EventHandler(this.Swap);
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::Puzzle.Properties.Resources.bg;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(598, 283);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(502, 275);
+            this.panel2.TabIndex = 3;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.Control;
+            this.button5.Location = new System.Drawing.Point(16, 143);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 70);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Shuffle";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.DarkGoldenrod;
+            this.button4.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(361, 143);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 63);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Step:";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(126, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 78);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PUZZLE";
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(187, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Undo";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::Puzzle.Properties.Resources.golden_retriever_dog_sits_blur_120469_3840x2160;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(598, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(502, 264);
+            this.panel1.TabIndex = 4;
+            // 
+            // frmPuzzelGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1141, 588);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.gbPuzzleBox);
+            this.Controls.Add(this.panel2);
+            this.Name = "frmPuzzelGame";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbPuzzleBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbPuzzleBox;
+        private System.Windows.Forms.PictureBox pbx1;
+        private System.Windows.Forms.PictureBox pbx2;
+        private System.Windows.Forms.PictureBox pbx3;
+        private System.Windows.Forms.PictureBox pbx4;
+        private System.Windows.Forms.PictureBox pbx5;
+        private System.Windows.Forms.PictureBox pbx6;
+        private System.Windows.Forms.PictureBox pbx7;
+        private System.Windows.Forms.PictureBox pbx8;
+        private System.Windows.Forms.PictureBox pbx9;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections;
+
+
+
+namespace Puzzle
+{
+    public partial class frmPuzzelGame : Form
+    {
+        int inNullSliceIndex;
+        int move = 0;
+        List<Bitmap> lstOriginalPictureList = new List<Bitmap>();
+        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+        Stack<int> st1 = new Stack<int>();
+        Stack<int> st2 = new Stack<int>();
+        Queue<int> que1 = new Queue<int>();
+
+
+        public frmPuzzelGame()
+        {
+            InitializeComponent();
+            lstOriginalPictureList.AddRange(new Bitmap[] { Properties.Resources._1, Properties.Resources._2, Properties.Resources._3, Properties.Resources._4, Properties.Resources._5, Properties.Resources._6, Properties.Resources._7, Properties.Resources._8, Properties.Resources._null });
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Shuffle();
+
+        }
+
+       void Shuffle()
+        {
+            st1.Clear();
+            st2.Clear();
+            que1.Clear();
+            for (int i = 0; i < 9; i++)
+            {
+                ((PictureBox)gbPuzzleBox.Controls[i]).Image = lstOriginalPictureList[i];
+
+            }
+            inNullSliceIndex = 8;
+
+            //First shuffle
+            List<int> move1st = new List<int>(new int[] { -3, -1 });
+            Random r1st = new Random();
+            int j1st;
+            j1st = move1st[r1st.Next(0, move1st.Count)] + inNullSliceIndex;
+            PictureBox temp = new PictureBox();
+            temp.Image = ((PictureBox)gbPuzzleBox.Controls[inNullSliceIndex]).Image;
+            ((PictureBox)gbPuzzleBox.Controls[inNullSliceIndex]).Image = ((PictureBox)gbPuzzleBox.Controls[j1st]).Image;
+            ((PictureBox)gbPuzzleBox.Controls[j1st]).Image = temp.Image;
+            inNullSliceIndex = j1st;
+            que1.Enqueue(inNullSliceIndex);
+            //shuffle
+            for (int i = 0; i < 9; i++)
+            {
+                int j;
+                if (inNullSliceIndex >= 6)
+                {
+                    if (inNullSliceIndex == 6)
+                    {
+                        List<int> move = new List<int>(new int[] { -3, 1 });
+                        j = Moveset(move);
+                    }
+                    else if (inNullSliceIndex == 8)
+                    {
+                        List<int> move = new List<int>(new int[] { -3, -1 });
+                        j = Moveset(move);
+                    }
+                    else
+                    {
+                        List<int> move = new List<int>(new int[] { -3, -1, 1 });
+                        j = Moveset(move);
+                    }
+                }
+                else if (inNullSliceIndex <= 2)
+                {
+                    if (inNullSliceIndex == 2)
+                    {
+                        List<int> move = new List<int>(new int[] { -1, 3 });
+                        j = Moveset(move);
+                    }
+                    else if (inNullSliceIndex == 0)
+                    {
+                        List<int> move = new List<int>(new int[] { 1, 3 });
+                        j = Moveset(move);
+                    }
+                    else
+                    {
+                        List<int> move = new List<int>(new int[] { -1, 1, 3 });
+                        j = Moveset(move);
+                    }
+                }
+                else
+                {
+                    if (inNullSliceIndex == 3)
+                    {
+                        List<int> move = new List<int>(new int[] { -3, 1, 3 });
+                        j = Moveset(move);
+                    }
+                    else if (inNullSliceIndex == 5)
+                    {
+                        List<int> move = new List<int>(new int[] { -3, -1, 3 });
+                        j = Moveset(move);
+                    }
+                    else
+                    {
+                        List<int> move = new List<int>(new int[] { -3, -1, 1, 3 });
+                        j = Moveset(move);
+                    }
+                }
+               
+                que1.Enqueue(inNullSliceIndex);
+                inNullSliceIndex = j;
+            }
+        }
+        private int Moveset(List<int> m)
+        {
+            int k = que1.Dequeue();
+            Random r = new Random();
+            int j;
+            do
+            {
+                j = m[r.Next(0, m.Count)] + inNullSliceIndex;
+            } while (j == k);
+            PictureBox temp = new PictureBox();
+            temp.Image = ((PictureBox)gbPuzzleBox.Controls[inNullSliceIndex]).Image;
+            ((PictureBox)gbPuzzleBox.Controls[inNullSliceIndex]).Image = ((PictureBox)gbPuzzleBox.Controls[j]).Image;
+            ((PictureBox)gbPuzzleBox.Controls[j]).Image = temp.Image;
+            return j;
+        }
+
+        private void Swap(object sender, EventArgs e)
+        {
+            int inPictureBoxIndex = gbPuzzleBox.Controls.IndexOf(sender as Control); ;
+            int a = (inPictureBoxIndex % 3 == 0) ? -1 : inPictureBoxIndex - 1;
+            int b = inPictureBoxIndex - 3;
+            int c = (inPictureBoxIndex % 3 == 2) ? -1 : inPictureBoxIndex + 1;
+            int d = inPictureBoxIndex + 3;
+
+            List<double> vitri = new List<double> { a, b, c, d };
+
+            if (vitri.Contains(inNullSliceIndex))
+            {
+                st1.Push(inNullSliceIndex);
+                PictureBox temp = new PictureBox();
+                temp.Image = ((PictureBox)gbPuzzleBox.Controls[inNullSliceIndex]).Image;
+                ((PictureBox)gbPuzzleBox.Controls[inNullSliceIndex]).Image = ((PictureBox)gbPuzzleBox.Controls[inPictureBoxIndex]).Image;
+                ((PictureBox)gbPuzzleBox.Controls[inPictureBoxIndex]).Image = temp.Image;
+                inNullSliceIndex = inPictureBoxIndex;
+                st2.Push(inPictureBoxIndex);
+                move++;
+                button4.Text = "Step:" + move;
+            }
+            if(Win()==true)
+            {
+                MessageBox.Show(" YOU WIN !!!!!   Step:  "+ move);
+               
+            }     
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Undo();
+        }
+        private void Undo()
+        {
+            if (st1.Count != 0 && st2.Count != 0)
+            {
+                move--;
+                button4.Text = "Step:" + move;
+                int i = st1.Pop();
+                int j = st2.Pop();
+
+                PictureBox temp = new PictureBox();
+                temp.Image = ((PictureBox)gbPuzzleBox.Controls[i]).Image;
+                ((PictureBox)gbPuzzleBox.Controls[i]).Image = ((PictureBox)gbPuzzleBox.Controls[j]).Image;
+                ((PictureBox)gbPuzzleBox.Controls[j]).Image = temp.Image;
+
+                inNullSliceIndex = i;
+            }
+            else MessageBox.Show("Không thể Undo");
+        }
+       
+        bool Win()
+        {
+            int i;
+            for(i=0;i<8;i++)
+            {
+                if (((PictureBox)gbPuzzleBox.Controls[i]).Image != lstOriginalPictureList[i])
+                    break;
+            }
+            if (i == 8) return true;
+            else return false;
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Shuffle();
+            move = 0;
+            button4.Text = "Step:" + move;
+        }
+    }
+}
